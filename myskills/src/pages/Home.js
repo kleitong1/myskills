d6beff69cd6cbd1518ac3b6a6b0daa96e94fd6ea
kleitong1 <44074264+kleitong1@users.noreply.github.com>
2022-02-12 15:42:56 -0300

    adicao de botoes e campos de input + listagem de habilidades

--- myskills/src/pages/Home.js
+++ myskills/src/pages/Home.js
@@ -1,28 +1,163 @@
-import React, { Fragment } from "react";
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity
+
+} from 'react-native';
 
 export function Home() {
+
+
+
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  // handle é usado quando uma funcao é disparada com alguma interação do usuario
+  function handleAddNewSkill() {
+    // atualiza o estado
+    setMySkills(oldState => [...oldState, newSkill]); // ... é chamado de spread operator: pega tudo do vetor
+
+
+  }
+
+
+
   return (
-    
-    <Fragment>
-      {/* //View seria uma equivalencia ao DIV */}
-      <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: "center"
-      }}>
-        {/* text é um elemento para conseguir colocar texto em tela */}
-        <Text> Oi </Text>
 
-      </View>
-      <Text style={{
 
-        alignSelf: 'center',
-        marginBottom: 20
-      }}>
-        opa
+    /* //View seria uma equivalencia ao DIV */
+
+
+    // atributo JSX 'style'
+    <View style={styles.container}>
+
+      {/* /** Text é um elemento para conseguir colocar texto em tela */}
+      <Text style={styles.title}>Bem-vindo, Kleiton</Text>
+
+
+      <TextInput
+        style={styles.input}
+        placeholder="Insira uma nova habilidade"
+        placeholderTextColor="#999"
+        onChangeText={setNewSkill}
+        
+
+
+
+
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={.7}
+        onPress={handleAddNewSkill}
+      >
+
+        <Text
+
+          style={styles.buttonText}>
+          Adicionar
+
         </Text>
-    </Fragment>
-    
+      </TouchableOpacity>
+
+      {/* adicionando marginVertical ao title apenas para o 'Minhas habilidades' */}
+      <Text style={[styles.title, { marginVertical: 50 }]}>
+
+        {/* {newSkill} */}
+        Minhas habilidades
+
+
+      </Text>
+
+      {
+
+        // map percorre cada elemento do myskills
+        mySkills.map(skill => (
+
+
+          <TouchableOpacity key={skill} style={styles.buttonSkill}>
+            <Text style={styles.textSkill}>
+
+
+              {skill}
+
+
+            </Text>
+          </TouchableOpacity>
+        ))
+      }
+
+    </View>
+
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    backgroundColor: '#121915',
+    paddingHorizontal: 30
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+
+  },
+
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: 15,
+
+    // se for IOS, o padding sera 15, caso contrario, sera 10
+    padding: Platform.OS === 'ios' ? 15 : 10,
+
+    marginTop: 30,
+    marginBottom: 15,
+    borderRadius: 7
+  },
+
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+
+  textSkill: {
+    color: '#fff',
+
+
+    fontSize: 18,
+    fontWeight: 'bold',
+
+  },
+
+
+  buttonSkill: {
+    backgroundColor: '#1F1E25',
+    padding: 20,
+    borderRadius: 15,
+    alignItems: 'center',
+    marginVertical: 10
+
+
+  }
+});
+
